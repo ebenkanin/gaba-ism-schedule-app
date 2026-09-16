65a37b09a1453faf0a52ac0feae4cfd5eb7e4126
ebenkanin <ebenezerkanin@gmail.com>
2026-09-17 01:11:57 +0900

    replacing route url from local host to render backend

--- main.js
+++ main.js
@@ -3,9 +3,25 @@ const container = document.querySelector('.schedule-container')
 
 const template = document.getElementById('template')
 
+const title_date = document.querySelector('.current-date')
+
+const managersOnSchedule = document.querySelector('.num_of_managers')
+
 
 const mobileNumber =document.querySelector('.mobile-num')
 const studioNumber = document.querySelector('.studio-num')
+
+const numberOnSchedule = document.querySelector('.number-on-schedule')
+
+const numberAvailable = document.querySelector('.number-available')
+
+
+
+
+
+ 
+
+
 
 
 // grabbing records from backend
@@ -27,7 +43,7 @@ const initiateRecordsRetrieval = async ()=>{
 
 // sending the date as the body of a request to the backend.
 
-    const response = await fetch('http://127.0.0.1:5000/',{method  :'POST',
+    const response = await fetch('https://ism-schedule-backend.onrender.com/',{method  :'POST',
         headers:{
             'Content-Type': 'application/JSON'
         },
@@ -37,12 +53,37 @@ const initiateRecordsRetrieval = async ()=>{
      
     })
 
+    //clearing the container and default text content of relevant elements
+
     container.innerHTML = " "
+
+    managersOnSchedule.textContent = " "
+
+    title_date.textContent=" "
+
+    numberAvailable.textContent =" "
+
+    numberOnSchedule.textContent = " "
+
+
+    //convert data from database to JSON
 
 
     const data = await response.json()
 
-    console.log(data)
+
+    //Appending counts of instructors on schedule and today's date to the DOM
+
+    title_date.textContent = `${dateToday.toLocaleDateString('en-US', { month: 'long' })} ${new Date().getDate()}`
+
+    managersOnSchedule.textContent = `${data.length}`
+
+
+
+    let numAvailable = 0
+    let numOnSchedule = 0
+
+    // Creating cards to be appended to the DOM
 
      const schduleCard = data.forEach(item => {
 
@@ -64,6 +105,8 @@ const initiateRecordsRetrieval = async ()=>{
         const status = card.querySelector('.status')
 
         const studio_name = card.querySelector('.studio-details')
+
+        const studioAccentContainer = card.querySelector('.initials-container')
 
 
         studioInitials.textContent = item.studio_initials
@@ -88,13 +131,64 @@ const initiateRecordsRetrieval = async ()=>{
 
         const minutes = timeNow.getMinutes()
 
-        console.log(currentTime, hour, minutes)
+        // Manager's start time
+        const [startHour, startMinutes] = item.shift_start.split(':')
+        const shiftStart =
+            Number(startHour) * 60 + Number(startMinutes)
+
+        // Manager's end time
+        const [endHour, endMinutes] = item.shift_end.split(':')
+        const shiftEnd =
+            Number(endHour) * 60 + Number(endMinutes)
+
+        // Determine status
+        if (shiftStart < currentTime && currentTime < shiftEnd) {
+
+            status.textContent = 'Available'
+            status.classList.add('bg-green-500')
+
+            numAvailable++
+
+        } else {
+
+            status.textContent = 'On schedule'
+            status.classList.add('bg-blue-500')
+
+            numOnSchedule++
+
+        }
+
+        if (studioAccentContainer){
+            studioAccentContainer.style.backgroundColor = getSoftRandomColor()
+        }
+
+        
+
+       
 
 
         container.append(card)
+
         
      });
+
+     numberAvailable.textContent = numAvailable
+
+     numberOnSchedule.textContent = numOnSchedule
 }
 
 
+
+
+function getSoftRandomColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = 35 + Math.floor(Math.random() * 15); // 35% - 50%
+    const lightness = 85 + Math.floor(Math.random() * 10);  // 85% - 95%
+
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+}
+
 document.addEventListener('DOMContentLoaded', initiateRecordsRetrieval)
+
+
+
